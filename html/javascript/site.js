3999e8b254a188edf3f9bcaec8761cdea61ef6c0
Airik Leon <airik.leon@gmail.com>
2017-11-01 10:54:31 -0600

    modifications to the index and then framed out the about...etc

---
+++ html/javascript/site.js
@@ -0,0 +1,9 @@
+function showSideBar(){
+    document.getElementById("nav-main").style.height = "100%";
+    document.getElementById("side-bar").style.visibility = "visible";
+
+}
+function hideSideBar(){
+    document.getElementById("nav-main").style.height= "60px"; 
+    document.getElementById("side-bar").style.visibility = "hidden"; 
+}

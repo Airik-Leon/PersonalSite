8fd905c48505ed97f825f218815308aabda9b286
Airik Leon <airik.leon@gmail.com>
2017-11-03 13:27:47 -0600

    updated with bootstrap

--- html/javascript/site.js
+++
@@ -1,9 +0,0 @@
-function showSideBar(){
-    document.getElementById("nav-main").style.height = "100%";
-    document.getElementById("side-bar").style.visibility = "visible";
-
-}
-function hideSideBar(){
-    document.getElementById("nav-main").style.height= "60px"; 
-    document.getElementById("side-bar").style.visibility = "hidden"; 
-}
